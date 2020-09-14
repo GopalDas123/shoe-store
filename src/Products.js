a8df941ebd6f0d594408b553094f0739213421af
GopalDas123 <Gopaldax@gmail.com>
2020-09-14 20:46:43 +0500

    Some CSS Issues Solved

--- src/Products.js
+++ src/Products.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(3),
     textAlign: "center",
-    height: "230px",
+    height: "280px",
     color: theme.palette.text.secondary,
     cursor: "pointer",
     "&:hover": {
